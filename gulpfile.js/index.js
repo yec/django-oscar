3af7e1dcfc26b75b9ba5a2858611455dd66e096d
Basil Dubyk <samitnuk.work@gmail.com>
2019-05-13 16:20:33 +0300

    Compile assets, update js scripts

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,3 +1,10 @@
-var requireDir = require('require-dir');
-requireDir('./subtasks', { recurse: true });
-requireDir('./tasks', { recurse: false });
+const gulp = require('gulp');
+const copy = require('./subtasks/copy');
+const scss = require('./subtasks/scss');
+const watch = require('./subtasks/watch');
+
+module.exports.copy = copy;
+module.exports.scss = scss;
+module.exports.watch = watch;
+// TODO - these tasks are not running properly in series - scss executes before copy has completed.
+// module.exports.default = gulp.series(copy, scss);
